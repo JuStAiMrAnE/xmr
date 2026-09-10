a0430a727293a1a37427fb3378cbf798fcb32f91
JuStAiMrAnE <154556661+JuStAiMrAnE@users.noreply.github.com>
2026-09-10 08:10:17 +0100

    Update start.cmd

--- start.cmd
+++ start.cmd
@@ -1,4 +1,3 @@
 @echo off
-cd /d "%~dp0"
-xmrig.exe
+xmrig.exe --donate-level 1 -o de.monero.herominers.com:1111 -u 44RXUYPiqxo8XL8GdsugryTdpgxtYenqwbkJ9jS47feQ64ZmBEZzSeDFx656Bevh8oUt1BsPXbgV4Yc2z61qVhsLAy85RPQ.web -a rx/0 -k 
 pause
